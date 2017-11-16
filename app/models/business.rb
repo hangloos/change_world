d34ec5eb1bc9797f1fbd5d21af28fa40a0be37f9
Eric Loos <ericloos00@gmail.com>
2017-11-16 15:29:23 -0600

    added items spec with test passing and items model

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,3 @@
 class Business < ApplicationRecord
+  validates :name, presence: true
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :item do
+    type ""
+    name "MyString"
+    price "9.99"
+  end
+end
